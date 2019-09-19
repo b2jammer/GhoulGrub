ad950be2df2d346efa1ddd8f36876bf89fa2440d
Ryan Mileris <ryanmileris@yahoo.com>
2019-09-19 17:29:44 -0600

    Added a keyboard controller and simple player movement

---
+++ Assets/_Scripts/Controllers/KeyboardInterfaceController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardInterfaceController : MonoBehaviour
+{
+    private Vector3 direction;
+
+    public Vector3 Direction {
+        get {
+            return direction;
+        }
+    }
+
+    private void Awake() {
+        direction = Vector3.zero;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckMovement();
+    }
+
+    private void CheckMovement() {
+        if (Input.GetButton("Horizontal")) {
+            direction.x = Input.GetAxisRaw("Horizontal");
+        }
+        else {
+            direction.x = 0;
+        }
+
+        if (Input.GetButton("Forward")) {
+            direction.z = Input.GetAxisRaw("Forward");
+        }
+        else {
+            direction.z = 0;
+        }
+
+        direction.Normalize();
+
+    }
+}
---
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public KeyboardInterfaceController keyboardInput;
+
+    public float speed = 5f;
+
+    private Vector3 direction;
+
+    private void Awake() {
+        direction = Vector3.zero;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = keyboardInput.Direction;
+        Move();
+    }
+
+    private void Move() {
+        transform.position += direction * speed * Time.deltaTime;
+    }
+}
