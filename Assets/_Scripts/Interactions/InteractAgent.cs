241ce7510078ad291921c0af0a0fc41530332a8f
Ben <brose376@gmail.com>
2019-09-25 14:04:09 -0600

    Implemented basic interactability, player now collides with tables.

---
+++ Assets/_Scripts/Interactions/InteractAgent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class InteractAgent : MonoBehaviour
+{
+    #region Private Variables
+    private Interactable currentInteractable;
+    #endregion
+
+    #region MonoBehaviour Methods
+    private void OnTriggerEnter(Collider other)
+    {
+        Interactable otherInteract;
+        if ((otherInteract = other.GetComponentInChildren<Interactable>()) != null)
+        {
+            currentInteractable = otherInteract;
+            otherInteract.OnApproach.Invoke();
+            Debug.Log("Approached.");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Interactable otherInteract;
+        if ((otherInteract = other.GetComponentInChildren<Interactable>()) != null && otherInteract == currentInteractable)
+        {
+            currentInteractable = null;
+            otherInteract.OnRetreat.Invoke();
+            Debug.Log("Retreated.");
+        }
+    }
+    #endregion
+
+    #region Script Specific Methods
+    public void Interact()
+    {
+        currentInteractable.OnInteract.Invoke();
+    }
+    #endregion
+}
---
+++ Assets/_Scripts/Interactions/Interactable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour
+{
+    #region Public Variables
+    public UnityEvent OnInteract;
+    public UnityEvent OnApproach;
+    public UnityEvent OnRetreat;
+    #endregion
+}
