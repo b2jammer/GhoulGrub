91ec33b0f9543d279b3f2366c0c0cdcc153b955a
Ryan Mileris <ryanmileris@yahoo.com>
2019-12-04 18:07:29 -0700

    added objective text

---
+++ Assets/_Scripts/UI/Objective.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Objective : MonoBehaviour
+{
+    private Text objectiveText;
+
+    private void Awake() {
+        objectiveText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetText();
+    }
+
+    void SetText() {
+        string objective = string.Format("Complete as many meals as possible before losing {0} meals \n", TentacularScore.Instance.maxLostMeals);
+        objective += string.Format("Meals lost: {0}/{1}", TentacularScore.Instance.LostMeals, TentacularScore.Instance.maxLostMeals);
+
+        objectiveText.text = objective;
+    }
+}
