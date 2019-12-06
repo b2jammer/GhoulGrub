e6b2547880876d6c33d53706bc26078ab3e6386d
SerNurp <55299287+SerNurp@users.noreply.github.com>
2019-12-05 23:26:10 -0700

    RecipeBook commit
    Added the recipe book UI, adjusted the Credits, and fixed the overlapping recipe text for the overlapping meals.

---
+++ Assets/RecipeBook.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecipeBook : MonoBehaviour
+{
+    public GameObject recipeBookIngredients, recipeBookIntermediateOne, recipeBookIntermediateTwo, recipeBookMealsOne, recipeBookMealsTwo;
+
+    public void IngredientsBookON()
+    {
+        recipeBookIngredients.SetActive(true);
+    }
+
+    public void IngredientsBookOFF()
+    {
+        recipeBookIngredients.SetActive(false);
+    }
+
+    public void InterOneBookON()
+    {
+        recipeBookIntermediateOne.SetActive(true);
+    }
+
+    public void InterOneBookOFF()
+    {
+        recipeBookIntermediateOne.SetActive(false);
+    }
+
+    public void InterTwoBookON()
+    {
+        recipeBookIntermediateTwo.SetActive(true);
+    }
+
+    public void InterTwoBookOFF()
+    {
+        recipeBookIntermediateTwo.SetActive(false);
+    }
+
+    public void MealsOneBookON()
+    {
+        recipeBookMealsOne.SetActive(true);
+    }
+
+    public void MealsOneBookOFF()
+    {
+        recipeBookMealsOne.SetActive(false);
+    }
+    public void MealsTwoBookON()
+    {
+        recipeBookMealsTwo.SetActive(true);
+    }
+
+    public void MealsTwoBookOFF()
+    {
+        recipeBookMealsTwo.SetActive(false);
+    }
+}
